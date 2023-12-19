fe8dbcb12e4c9c5f318a01cc1719d75b50ed6a7c
jkmathilda <jkmathilda627@gmail.com>
2023-12-18 21:29:24 -0800

    Created PDF uploading section using streamlit

--- app.py
+++ app.py
@@ -1,9 +1,12 @@
 from dotenv import load_dotenv
-import os
+import streamlit as st
 
 def main():
     load_dotenv()
-    print(os.getenv("OPENAI_API_KEY"))
+    st.set_page_config(page_title="Ask your PDF")
+    st.header("Ask your PDF")
+    
+    pdf = st.file_uploader("Upload your PDF", type="pdf")
 
 if __name__ == '__main__':
     main()
