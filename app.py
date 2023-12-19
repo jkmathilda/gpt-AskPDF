0eb70d61ff11bd7a462d5f82bc6abf735be135a7
jkmathilda <jkmathilda627@gmail.com>
2023-12-18 22:18:44 -0800

    Uploading a pdf file and extracting the pdf file

--- app.py
+++ app.py
@@ -1,12 +1,22 @@
 from dotenv import load_dotenv
 import streamlit as st
+from PyPDF2 import PdfReader
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="Ask your PDF")
     st.header("Ask your PDF")
     
+    # upload file
     pdf = st.file_uploader("Upload your PDF", type="pdf")
+    
+    # extract the text
+    if pdf is not None:
+        pdf_reader = PdfReader(pdf)
+        text = ""
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+        st.write(text)
 
 if __name__ == '__main__':
     main()
